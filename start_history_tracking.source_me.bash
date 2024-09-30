54999e229c5be9af17b086ef2df6987b72fa2040
Bryna Hazelton <bryna.hazelton@gmail.com>
2024-09-30 14:20:32 -0700

    update the bash command for Bryna

--- start_history_tracking.source_me.bash
+++ start_history_tracking.source_me.bash
@@ -1,3 +1,3 @@
 #!/bin/bash
-export HISTFILE='/home/dacb/history/history_583.txt'
-export PROMPT_COMMAND="history -a; history -c; history -r; pushd . &> /dev/null; cd ~/history_583; git add history.txt; git commit --quiet -m 'updating' &> /dev/null; git push --quiet &> /dev/null; popd &> /dev/null; $PROMPT_COMMAND"
+export HISTFILE='/Users/bryna/Projects/teaching/cse583/history/history.txt'
+export PROMPT_COMMAND="history -a; history -c; history -r; pushd . &> /dev/null; cd /Users/bryna/Projects/teaching/cse583/history; git add history.txt; git commit --quiet -m 'updating' &> /dev/null; git push --quiet &> /dev/null; popd &> /dev/null; $PROMPT_COMMAND"
